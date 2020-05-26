259d3f9348ff36e19c32b6120779ff614d2ac808
Nguyễn Trung Hiếu <trunghieu99tt@gmail.com>
2020-05-26 15:05:43 +0700

    Updates

---
+++ precache-manifest.aada571f016f31874670c7cbe641cef5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79378b2aa0f57797f789c8dbece357f6",
+    "url": "/Messenger-Chat-App/index.html"
+  },
+  {
+    "revision": "44163ea145e174f12619",
+    "url": "/Messenger-Chat-App/static/css/2.7f39cfac.chunk.css"
+  },
+  {
+    "revision": "0e1b2ad121b9e386def4",
+    "url": "/Messenger-Chat-App/static/css/main.001611bd.chunk.css"
+  },
+  {
+    "revision": "44163ea145e174f12619",
+    "url": "/Messenger-Chat-App/static/js/2.53d46a1e.chunk.js"
+  },
+  {
+    "revision": "eccec3a151c0bbb8804c6fca25ca4140",
+    "url": "/Messenger-Chat-App/static/js/2.53d46a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1b2ad121b9e386def4",
+    "url": "/Messenger-Chat-App/static/js/main.5f6d563a.chunk.js"
+  },
+  {
+    "revision": "6476758f6490b32db280",
+    "url": "/Messenger-Chat-App/static/js/runtime-main.9366f082.js"
+  },
+  {
+    "revision": "5097b0247a92d47178df598b82944f15",
+    "url": "/Messenger-Chat-App/static/media/default-avatar.5097b024.png"
+  },
+  {
+    "revision": "98d17d5f06bc7d76b808db69ad14d9f1",
+    "url": "/Messenger-Chat-App/static/media/register.98d17d5f.jpg"
+  }
+]);
